70eed2510c5b7510af95086537d1465be6509cdb
Andrea Pavoni <andrea.pavoni@gmail.com>
2013-04-26 16:30:57 +0200

    fix class namespace for Rails generator

--- lib/generators/easy_config/install/install_generator.rb
+++ lib/generators/easy_config/install/install_generator.rb
@@ -1,20 +1,23 @@
 require 'rails/generators/base'
 
-class InstallGenerator < ::Rails::Generators::Base #:nodoc:
+module EasyConfig
+  class InstallGenerator < ::Rails::Generators::Base #:nodoc:
 
-  desc 'Generates EasyConfig files.'
+    desc 'Generates EasyConfig files.'
 
-  def self.source_root
-    @_easy_config_source_root ||= File.expand_path(File.join(File.dirname(__FILE__), 'templates'))
-  end
+    def self.source_root
+      @_easy_config_source_root ||= File.expand_path(File.join(File.dirname(__FILE__), 'templates'))
+    end
 
-  def self.banner
-    "rails generate easy_config:install"
-  end
+    def self.banner
+      "rails generate easy_config:install"
+    end
 
-  def copy_files
-    template 'easy_config.rb', 'config/initializers/easy_config.rb'
-    template 'easy_config.yml', 'config/easy_config.yml'
+    def copy_files
+      template 'easy_config.rb', 'config/initializers/easy_config.rb'
+      template 'easy_config.yml', 'config/easy_config.yml'
+    end
+
   end
 
 end
